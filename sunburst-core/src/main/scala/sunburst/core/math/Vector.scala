d8b36d9daa54a55fb3fe21697d5909858b89934d
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2021-10-03 01:12:46 +0200

    use AnyWordSpec

--- sunburst-core/src/main/scala/sunburst/core/math/Vector.scala
+++ sunburst-core/src/main/scala/sunburst/core/math/Vector.scala
@@ -1,8 +1,15 @@
 package sunburst.core.math
 
 case class Vector private (val x: Float, val y: Float, val z: Float):
-  def +(other: Vector) = Vector(x + other.x, y + other.y, z + other.z)
-  def *(scalar: Float) = Vector(x * scalar, y * scalar, z * scalar)
+  def +(v: Vector): Vector     = Vector(x + v.x, y + v.y, z + v.z)
+  def -(v: Vector): Vector     = Vector(x - v.x, y - v.y, z - v.z)
+  def *(scalar: Float): Vector = Vector(x * scalar, y * scalar, z * scalar)
+  def /(scalar: Float): Vector =
+    if scalar == 0.0f then throw new java.lang.ArithmeticException("/ by zero")
+    this * (1.0f / scalar)
+
+  def dot(v: Vector): Float = x * v.x + y * v.y + z * v.z
+  def length: Float         = math.sqrt(dot(this)).toFloat
 
 case object Vector:
   def apply(s: Float)                     = new Vector(s, s, s)
--- sunburst-core/src/test/scala/sunburst/core/math/VectorSpec.scala
+++ sunburst-core/src/test/scala/sunburst/core/math/VectorSpec.scala
@@ -1,21 +1,50 @@
 package sunburst.core.math
 
 import org.scalatest.*
-import flatspec.*
+import wordspec.*
 import matchers.*
 
-class VectorSpec extends AnyFlatSpec with should.Matchers:
-  "A Vector" should "be commutatively additive" in {
-    val v1 = Vector(1, 2, 3)
-    val v2 = Vector(4, 5, 6)
+class VectorSpec extends AnyWordSpec with should.Matchers:
+  "A Vector" should {
 
-    val res1 = v1 + v2
-    val res2 = v2 + v1
-    res1 shouldBe res2
-  }
-  it should "be scalable with a constant factor" in {
-    val v1 = Vector(1, 2, 3)
-    val v2 = v1 * 5
+    "be commutatively additive" in {
+      val v1 = Vector(1, 2, 3)
+      val v2 = Vector(4, 5, 6)
 
-    v2 shouldBe Vector(5, 10, 15)
+      v1 + v2 shouldBe v2 + v1
+    }
+
+    "be subtractive" in {
+      val v1 = Vector(1, 2, 3)
+      val v2 = Vector(4, 5, 6)
+
+      v1 - v2 shouldBe Vector(-3)
+      v2 - v1 shouldBe Vector(3)
+    }
+
+    "be scalable by a constant factor" in {
+      val v1 = Vector(1, 2, 3)
+
+      v1 * 5 shouldBe Vector(5, 10, 15)
+    }
+
+    "be divisible by a constant factor" in {
+      val v1 = Vector(1, 2, 3)
+
+      v1 / 5 shouldBe Vector(0.2f, 0.4f, 0.6f)
+      intercept[java.lang.ArithmeticException] {
+        v1 / 0
+      }
+    }
+
+    "be able to calculate a dot product" in {
+      Vector(3, 4, 0).dot(Vector(3, 4, 0)) shouldBe 25
+      Vector(3, 4, 0).dot(Vector(1, 2, 3)) shouldBe 11
+    }
+
+    "have a length" in {
+      Vector(3, 4, 0).length shouldBe 5
+      Vector(1, 2, 3).length shouldBe 3.74f +- 0.01f
+    }
+
   }
